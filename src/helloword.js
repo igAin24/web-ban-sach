6adc712b667976d1c20b560d17e84f09532292c1
phuoc anh <phuocanhtieu@gmail.com>
2024-01-02 21:33:16 +0900

    đã fix ảnh

--- src/helloword.js
+++ src/helloword.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
   res.render("view");
